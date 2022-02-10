e63f4ff8d854e40cab0995f902a382e094616daf
Marwa Adel <marwaayosiif@gmail.com>
2022-02-10 21:20:29 +0200

    register controller

--- Code/ChattingApp/Client/src/main/java/gov/iti/jets/presentation/controllers/RegisterController.java
+++ Code/ChattingApp/Client/src/main/java/gov/iti/jets/presentation/controllers/RegisterController.java
@@ -3,9 +3,11 @@ package gov.iti.jets.presentation.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import gov.iti.jets.presentation.util.StageCoordinator;
 
 public class RegisterController {
-
+    StageCoordinator stageCoordinator = StageCoordinator.getInstance();
     @FXML
     private Label female;
 
@@ -14,5 +16,10 @@ public class RegisterController {
 
     @FXML
     private Button regbutton;
+
+    @FXML
+    void registerOnMouseClick(MouseEvent event) {
+        stageCoordinator.switchToLoginScreen();
+    }
 
 }
