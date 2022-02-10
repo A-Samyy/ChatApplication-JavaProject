2dc0407043570f8bd97f97b8f814b1af3a87aea2
Samy <asamy97@gmail.com>
2022-02-10 17:13:03 +0200

    updating RegisterController

--- Code/ChattingApp/Client/src/main/java/gov/iti/jets/presentation/controllers/RegisterController.java
+++ Code/ChattingApp/Client/src/main/java/gov/iti/jets/presentation/controllers/RegisterController.java
@@ -1,21 +1,18 @@
 package gov.iti.jets.presentation.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 public class RegisterController {
 
     @FXML
-    private Pane leftPane;
+    private Label female;
+
+    @FXML
+    private Label male;
 
     @FXML
     private Button regbutton;
-
-    @FXML
-    void onRegisterClick(ActionEvent event) {
-
-    }
 
 }
