7502b73b52995065b21042883d9a9be9b1a3bfb3
Samy <asamy97@gmail.com>
2022-02-12 11:12:10 +0200

    test branch

--- Code/ChattingApp/Server/src/main/java/gov/iti/jets/MainApp.java
+++ Code/ChattingApp/Server/src/main/java/gov/iti/jets/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
         stageCoordinator.initStage(primaryStage);
         stageCoordinator.switchToLoginScreen();
         primaryStage.show();
