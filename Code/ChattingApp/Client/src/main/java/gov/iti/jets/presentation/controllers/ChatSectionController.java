e48b7d6c7d9316c8c97b68695396c05b8a78edc2
HendElAhmadi <1998hend@gmail.com>
2022-02-18 14:35:09 +0200

    loading chat Section to work on message

--- Code/ChattingApp/Client/src/main/java/gov/iti/jets/presentation/controllers/ChatSectionController.java
+++ Code/ChattingApp/Client/src/main/java/gov/iti/jets/presentation/controllers/ChatSectionController.java
@@ -2,9 +2,11 @@ package gov.iti.jets.presentation.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 public class ChatSectionController {
 
@@ -12,7 +14,16 @@ public class ChatSectionController {
     private AnchorPane bottomBar;
 
     @FXML
+    private FontIcon filesButton;
+
+    @FXML
     private SplitMenuButton htmlEditor;
+
+    @FXML
+    private TextField messageTextFiel;
+
+    @FXML
+    private FontIcon sendButton;
 
     @FXML
     private AnchorPane topBar;
