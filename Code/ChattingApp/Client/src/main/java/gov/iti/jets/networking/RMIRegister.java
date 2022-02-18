3012bfea9fb3d387b64293f5901dffb47e1da082
Samy <asamy97@gmail.com>
2022-02-18 10:44:08 +0200

    solving conflict

--- Code/ChattingApp/Client/src/main/java/gov/iti/jets/networking/RMIRegister.java
+++ Code/ChattingApp/Client/src/main/java/gov/iti/jets/networking/RMIRegister.java
@@ -15,7 +15,7 @@ public class RMIRegister {
 
     private RMIRegister() {
         try {
-            registry = LocateRegistry.getRegistry("localhost", 5001);
+            registry = LocateRegistry.getRegistry("localhost", 4001);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
