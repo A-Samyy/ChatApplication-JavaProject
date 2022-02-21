ef34aa7a3586eb787b6a9b2cb4d557d17629b26f
Abdelazizeldahshan <abdelazizeldahshan5@gmail.com>
2022-02-21 20:13:51 +0200

    nuw pull

--- Code/ChattingApp/Server/src/main/java/gov/iti/jets/presistance/util/Connector.java
+++ Code/ChattingApp/Server/src/main/java/gov/iti/jets/presistance/util/Connector.java
@@ -17,11 +17,11 @@ public class Connector {
 
     static {
         HikariConfig config =new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3307/chatting_app");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/chatting_app");
         config.setUsername("root");
-        config.setPassword("hend1234");
+        config.setPassword("abdelaziz123@!$");
         dataSource= new HikariDataSource( config );
-        dataSource.setMaximumPoolSize(10);
+        dataSource.setMaximumPoolSize(20);
     }
 
     private static Connector connector = new Connector();
