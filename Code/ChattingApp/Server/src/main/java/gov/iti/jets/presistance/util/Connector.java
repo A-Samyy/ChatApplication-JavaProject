cf26855f11b1c8f244e317de2421646dd1f7837e
Samy <asamy97@gmail.com>
2022-02-21 07:59:32 +0200

    connector update

--- Code/ChattingApp/Server/src/main/java/gov/iti/jets/presistance/util/Connector.java
+++ Code/ChattingApp/Server/src/main/java/gov/iti/jets/presistance/util/Connector.java
@@ -17,9 +17,9 @@ public class Connector {
 
     static {
         HikariConfig config =new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3307/chatting_app");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/iti");
         config.setUsername("root");
-        config.setPassword("hend1234");
+        config.setPassword("rootpassword");
         dataSource= new HikariDataSource( config );
         dataSource.setMaximumPoolSize(20);
     }
