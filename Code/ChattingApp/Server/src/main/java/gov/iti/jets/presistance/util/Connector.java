4fd9f54b2fcdc7fbee8c3d33c13a8a36e9eeecb2
Samy <asamy97@gmail.com>
2022-02-25 13:54:12 +0200

    hiding ds.properties and updating RMIRegister

--- Code/ChattingApp/Server/src/main/java/gov/iti/jets/presistance/util/Connector.java
+++ Code/ChattingApp/Server/src/main/java/gov/iti/jets/presistance/util/Connector.java
@@ -1,9 +1,6 @@
 package gov.iti.jets.presistance.util;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.sql.Connection;
@@ -14,17 +11,26 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class Connector {
     private static HikariDataSource dataSource;
-
+    private static FileInputStream fis = null;
+    private static Properties prop = new Properties();
     static {
-        HikariConfig config =new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/chatting_app");
-        config.setUsername("marwa");
-        config.setPassword("Ma19971993");
-        dataSource= new HikariDataSource( config );
-        dataSource.setMaximumPoolSize(50);
-    }
+        HikariConfig config = new HikariConfig();
+        try {
+            fis = new FileInputStream("db.properties");
+            prop.load(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        config.setJdbcUrl(prop.getProperty("MYSQL_DB_URL"));
+        config.setUsername(prop.getProperty("MYSQL_DB_USERNAME"));
+        config.setPassword(prop.getProperty("MYSQL_DB_PASSWORD"));
+        dataSource = new HikariDataSource(config);
+        dataSource.setMaximumPoolSize(100);
 
-    private static Connector connector = new Connector();
+    }
+        private static Connector connector = new Connector();
     private Connector() {
     }
 
