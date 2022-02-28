3431009a6cdc7e077737614385663072ddd24499
Samy <asamy97@gmail.com>
2022-02-28 11:35:16 +0200

    Logout feature done again

--- Code/ChattingApp/Client/src/main/java/gov/iti/jets/service/services/LogoutService.java
+++ Code/ChattingApp/Client/src/main/java/gov/iti/jets/service/services/LogoutService.java
@@ -5,6 +5,8 @@ import gov.iti.jets.common.interfaces.ServerGroupChatMessageInt;
 import gov.iti.jets.common.interfaces.ServerMessageAnnouncetInt;
 import gov.iti.jets.common.interfaces.ServerMessageInt;
 import gov.iti.jets.networking.RMIRegister;
+import gov.iti.jets.presentation.controllers.ChatSectionController;
+import gov.iti.jets.presentation.controllers.SidebarController;
 import gov.iti.jets.service.impl.ClientAnnounceImpl;
 import gov.iti.jets.service.impl.ClientFileRequestImpl;
 import gov.iti.jets.service.impl.ClientGroupChatMessageImpl;
@@ -18,8 +20,8 @@ public class LogoutService {
     private ServerGroupChatMessageInt serverGroupChatMessageInt = rmiRegister.groupChatMessageService();
     private ServerMessageAnnouncetInt serverMessageAnnouncetInt = rmiRegister.serverMessageAnnouncetInt();
     private ServerMessageInt serverMessageInt = rmiRegister.messageService();
+    MessageService messageService = MessageService.getInstance();
     private int userId = LoginService.getId();
-
     public LogoutService() {
     }
 
@@ -29,6 +31,9 @@ public class LogoutService {
             serverMessageAnnouncetInt.unRegister(ClientAnnounceImpl.getClientAnnounce());
             serverGroupChatMessageInt.unregister(ClientGroupChatMessageImpl.getClientGroupChatMessage(),userId);
             serverFileRequestInt.unRegister(ClientFileRequestImpl.getClientFileRequest(), userId);
+            messageService.list.clear();
+            messageService.list1.clear();
+            messageService.getClient().removeMe();
         } catch (RemoteException e) {
             e.printStackTrace();
         }
